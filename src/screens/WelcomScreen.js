cd9246af82fcfe5881bd49e92315a82ede738abe
buiqtuan <tuanbq2909@gmail.com>
2018-07-03 00:11:44 +0700

    first commit

---
+++ src/screens/WelcomScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View} from 'react-native';
+
+class WelcomeScreen extends React.Component {
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+const welcomScreenStyles = {
+
+};
+
+export default WelcomeScreen;
